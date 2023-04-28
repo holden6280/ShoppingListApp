1e43acf391c8ea125a691aac45260d38a1cd5a94
holden6280 <emiicleghorn@gmail.com>
2023-04-28 16:18:27 -0400

    Only commit

---
+++ src/main/java/com/learntocode/Person.java
@@ -0,0 +1,30 @@
+package com.learntocode;
+
+public class Person {
+
+    String name = "";
+
+    public Person (String name) {
+        this.name = name;
+
+    }
+
+    public void sayHello () {
+
+        System.out.println("Hello, my name is " + this.name);
+
+    }
+
+    public void ShoppingList () {
+
+        System.out.println("desk");
+        System.out.println("napkin rings");
+        System.out.println("pajamas");
+        System.out.println("screen door lock");
+        System.out.println("tablecloth");
+        System.out.println("table runner");
+        System.out.println("shoes");
+        System.out.println("chicken");
+
+    }
+}
---
+++ src/main/java/com/learntocode/ShoppingListApp.java
@@ -0,0 +1,8 @@
+package com.learntocode;
+public class ShoppingListApp {
+    public static void main(String[] args) {
+        Person holden = new Person ("Holden");
+        holden.sayHello();
+        holden.ShoppingList();
+    }
+}
